2fefb1b219184dcceba8693e896696fe8521a98d
Pedro Costa <pcon1996@gmail.com>
2020-02-13 08:10:53 -0300

    Salvando dos dados do usuário logado no localStorage

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -3,6 +3,8 @@ import { CredenciaisDTO } from "../models/credenciais.dto";
 import { HttpClient } from "@angular/common/http";
 import { API_CONFIG } from "../config/api.config";
 import { Platform } from "ionic-angular";
+import { LocalUser } from "../models/local_user";
+import { StorageService } from "./storage.service";
 
 @Injectable()
 export class AuthService {
@@ -11,7 +13,8 @@ export class AuthService {
 
     constructor(
         public http: HttpClient,
-        private _platform: Platform
+        private _platform: Platform,
+        public storageServer: StorageService
         ){
 
             if(this._platform.is("cordova")){
@@ -19,7 +22,7 @@ export class AuthService {
             }
     }
 
-    authenticate(creds : CredenciaisDTO){
+    authenticate(creds : CredenciaisDTO) {
         return this.http.post(
             `${this.basePath}/login`, 
             creds,
@@ -27,5 +30,18 @@ export class AuthService {
                 observe: 'response',
                 responseType: 'text'
             });
+    }
+
+    successfullLogin(authorizationValue: String) {
+        let token = authorizationValue.substring(7);
+        let user : LocalUser = {
+            token: token
+        };
+        
+        this.storageServer.setLocalUser(user);
+    }
+
+    logout() {
+        this.storageServer.setLocalUser(null);
     }
 }
